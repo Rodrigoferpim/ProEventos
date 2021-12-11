507d31efb4b6ac2d5eb158d4b377b1cb475b4b40
Rodrigo Fernandes <rodrigo-fern@hotmail.com>
2021-12-11 00:43:14 -0300

    Primeira versão do backend

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(5).ToString()
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "2º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(10).ToString()
+            }
+        };
+
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}

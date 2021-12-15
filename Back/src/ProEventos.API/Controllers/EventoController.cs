2ac5b7451848f56244d4a6195af7186289745135
Rodrigo Fernandes <rodrigo-fern@hotmail.com>
2021-12-14 22:39:33 -0300

    Criação do banco de dados + EF

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,41 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "São Paulo",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(5).ToString()
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 5",
-                Local = "São Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(10).ToString()
-            }
-        };
+        private readonly DataContext _context;
 
-
-        public EventoController()
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
+
+
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
     }
 }
